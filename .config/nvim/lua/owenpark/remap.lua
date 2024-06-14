cfd4b61ebb08c47b8683ed3cd9c942307c7fce79
Owen Park <owenmmp@gmail.com>
2024-06-14 15:24:44 -0400

    feat: escape terminal mode easier

--- .config/nvim/lua/owenpark/remap.lua
+++ .config/nvim/lua/owenpark/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Keybinds to make split navigation easier.
+-- Keybinds to make split navigation easier
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
@@ -11,3 +11,7 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- NOTE: This won't work in all terminal emulators/tmux/etc
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
